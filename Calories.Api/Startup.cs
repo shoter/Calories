aba3e1f9a2bc2c69406de4fab34969e254a16835
shoter <lubiebardzozelki@gmail.com>
2018-07-14 17:53:28 +0200

    Basic project structure

--- Calories.Api/Startup.cs
+++ Calories.Api/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Calories
+namespace Calories.Api
 {
     public class Startup
     {
