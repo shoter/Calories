54bb3b4cb1e39253bd4c669ca334ddd73852034f
Damian Laczak <shoter@users.noreply.github.com>
2021-12-13 02:26:15 +0100

    feat: url from origin

--- Calories.Web/src/Api/CaloriesApiBase.ts
+++ Calories.Web/src/Api/CaloriesApiBase.ts
@@ -3,6 +3,8 @@ import { ApiBase } from "./ApiBase";
 export class CaloriesApiBase extends ApiBase {
     constructor(controllerName: string) {
         const url : string = (process.env.REACT_APP_BACKEND_HOST as string); 
-        super("http://localhost:19999/", controllerName); 
+        let ip = window.location.host.split(":")[0];
+        let baseUrl = "http://" + ip + ":19999";
+        super(baseUrl, controllerName); 
     }
 }
